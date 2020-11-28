8b91f7674d4868dc16ee552d981ac6424e5c965c
ZmjDns <292570927@qq.com>
2020-11-28 21:33:17 +0800

    动态设置GridView，并调整item的宽高比例

--- lib/components/GridViewComp.dart
+++ lib/components/GridViewComp.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_androidx/res/ListData.dart';
 
 class GridViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GridViewComp1(),
+      child: DynamicGridView1(),//GridViewComp1(),
     );
   }
 }
@@ -41,4 +43,33 @@ class GridViewComp1 extends StatelessWidget {
       ],
     );
   }
+}
+/**
+ * 动态设置GridView，并调整item的宽高比例
+ */
+class DynamicGridView1 extends StatelessWidget {
+
+  List<Widget> _getGridViewItem (){
+
+    return listData.map((e) =>
+        Container(
+          alignment: Alignment.center,
+          child: Text(e['title']),
+          color: Colors.yellow,
+        ))
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,  //每行的列数
+      mainAxisSpacing: 10,//主轴上下两行的间距
+      crossAxisSpacing: 10,//交叉轴每列左右间距
+      padding: EdgeInsets.all(10),//设置外边距
+      childAspectRatio: 0.7,    //调整子容器宽高比例（直接设置宽高不起作用）
+      children: _getGridViewItem(),
+    );
+  }
+
 }
