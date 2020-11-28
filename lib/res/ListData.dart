85fe728d0c58367bb75c7a09bd1e6d5ce754a29b
ZmjDns <292570927@qq.com>
2020-11-28 20:01:05 +0800

    引入外部数据，动态实现图文列表

---
+++ lib/res/ListData.dart
@@ -0,0 +1,22 @@
+List listData = [
+  {
+    "title": 'title1',
+    "author": 'aauthor1',
+    "imageUrl": 'https://www.itying.com/images/flutter/1.png'
+  },
+  {
+    "title": 'title2',
+    "author": 'aauthor2',
+    "imageUrl": 'https://www.itying.com/images/flutter/2.png'
+  },
+  {
+    "title": 'title3',
+    "author": 'aauthor3',
+    "imageUrl": 'https://www.itying.com/images/flutter/3.png'
+  },
+  {
+    "title": 'title4',
+    "author": 'aauthor4',
+    "imageUrl": 'https://www.itying.com/images/flutter/4.png'
+  }
+];
