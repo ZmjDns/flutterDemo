26af6d8c098f1970ef13de94b7bb459d3647117d
ZmjDns <292570927@qq.com>
2020-11-28 21:58:30 +0800

    用Colum实现GridView中item垂直布局

--- lib/components/GridViewComp.dart
+++ lib/components/GridViewComp.dart
@@ -6,7 +6,7 @@ class GridViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: DynamicGridView1(),//GridViewComp1(),
+      child: DynamicGridView2(),//DynamicGridView1(),//GridViewComp1(),
     );
   }
 }
@@ -69,6 +69,43 @@ class DynamicGridView1 extends StatelessWidget {
       padding: EdgeInsets.all(10),//设置外边距
       childAspectRatio: 0.7,    //调整子容器宽高比例（直接设置宽高不起作用）
       children: _getGridViewItem(),
+    );
+  }
+
+}
+
+/**
+ * 用Colum实现GridView中item垂直布局
+ */
+class DynamicGridView2 extends StatelessWidget {
+
+  List<Widget> _getGridViewDataList() {
+    var tempList = listData.map((e) => _gridViewItem(e));
+
+    return tempList.toList();
+  }
+
+  Widget _gridViewItem(e) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.yellow),
+      child: Column(
+        children: [
+          Image.network(e['imageUrl']),
+          Text(e['title'])
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 10,
+      padding: EdgeInsets.all(10),
+      childAspectRatio: 0.7,
+      children: _getGridViewDataList(),
     );
   }
 
