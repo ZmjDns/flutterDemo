c8bb82914aa385311298c3bf8342e53f49750a18
ZmjDns <292570927@qq.com>
2020-11-29 10:51:18 +0800

    补充数据

--- lib/res/ListData.dart
+++ lib/res/ListData.dart
@@ -18,5 +18,20 @@ List listData = [
     "title": 'title4',
     "author": 'aauthor4',
     "imageUrl": 'https://www.itying.com/images/flutter/4.png'
+  },
+  {
+    "title": 'title5',
+    "author": 'aauthor5',
+    "imageUrl": 'https://www.itying.com/images/flutter/5.png'
+  },
+  {
+    "title": 'title6',
+    "author": 'aauthor6',
+    "imageUrl": 'https://www.itying.com/images/flutter/6.png'
+  },
+  {
+    "title": 'title7',
+    "author": 'aauthor7',
+    "imageUrl": 'https://www.itying.com/images/flutter/7.png'
   }
 ];
