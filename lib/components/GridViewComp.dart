f8c619512183e33b91f8316d2345526cfdcac3fe
ZmjDns <292570927@qq.com>
2020-11-29 11:37:55 +0800

    PaddingRow 组件结合GridView

--- lib/components/GridViewComp.dart
+++ lib/components/GridViewComp.dart
@@ -7,7 +7,8 @@ class GridViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: DynamicGridView3(),//DynamicGridView2(),//DynamicGridView1(),//GridViewComp1(),
+      child: DynamicGridView4(),
+      //DynamicGridView3(),//DynamicGridView2(),//DynamicGridView1(),//GridViewComp1(),
     );
   }
 }
@@ -162,6 +163,54 @@ class DynamicGridView3 extends StatelessWidget {
 
 }
 
+/**
+ * PaddingRow 组件结合GridView
+ */
+class DynamicGridView4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Image.network('https://www.itying.com/images/flutter/1.png',fit: BoxFit.cover,),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Image.network('https://www.itying.com/images/flutter/7.png',fit: BoxFit.cover,),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Image.network('https://www.itying.com/images/flutter/6.png',fit: BoxFit.cover,),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Image.network('https://www.itying.com/images/flutter/5.png',fit: BoxFit.cover,),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Image.network('https://www.itying.com/images/flutter/4.png',fit: BoxFit.cover,),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Image.network('https://www.itying.com/images/flutter/3.png',fit: BoxFit.cover,),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Image.network('https://www.itying.com/images/flutter/2.png',fit: BoxFit.cover,),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Image.network('https://www.itying.com/images/flutter/1.png',fit: BoxFit.cover,),
+          ),
+        ],
+      ),
+    );
+  }
 
+}
 
 
