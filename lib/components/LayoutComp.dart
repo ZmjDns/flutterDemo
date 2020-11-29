192d2c8f95b8f8b6bef593980468e4d93fef6d6c
ZmjDns <292570927@qq.com>
2020-11-29 13:37:31 +0800

    icon封装自定义样式

--- lib/components/LayoutComp.dart
+++ lib/components/LayoutComp.dart
@@ -4,22 +4,38 @@ import 'package:flutter/material.dart';
 class LayoutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return IconContainer();
+    return IconContainer(Icons.home,color: Colors.black87,);
   }
 }
 
 /**
  * icon简单实现
+ * icon封装
+ * 必选参数在构造器参数列表中正常声明
+ * 可选参数必选在参数列表中用{}包括
  */
 class IconContainer extends StatelessWidget {
+  double size;
+  Color color;
+  IconData icon;
+
+  /**
+   * [icon]必选
+   * [color]和[size] 可选参数，用花括号抱起来
+   * 默认参数必须要在构造器中赋值
+    */
+  IconContainer(this.icon,{this.color,this.size}){
+    this.size = 32.0;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 100,
       width: 100,
-      color: Colors.yellow,
+      color: this.color,
       child: Center(
-        child: Icon(Icons.home,size: 32,color: Colors.yellowAccent,),
+        child: Icon(icon,size: size,color: Colors.white,),
       ),
     );
   }
