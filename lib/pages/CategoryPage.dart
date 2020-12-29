11b3841bf9c38659eb501300fa627f77cf1ba967
ZmjDns <292570927@qq.com>
2020-12-29 23:03:30 +0800

    单个Tab页中实现顶部Tab

--- lib/pages/CategoryPage.dart
+++ lib/pages/CategoryPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_androidx/components/tab/Tabs.dart';
 import 'package:flutter_app_androidx/pages/FormPage.dart';
 
 class CategoryPage extends StatefulWidget {
@@ -10,29 +11,75 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        RaisedButton(
-          child: Text('进入表单界面'),
-          onPressed: (){
-            /*Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => FormPage(
-                  title: '我是传值数据',
-                )
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              Expanded(
+                child: TabBar(
+                  tabs: [
+                    Tab(text: '热销',),
+                    Tab(text: '推荐',),
+                    Tab(text: '直播',),
+                    Tab(text: '社区',)
+                  ],
+                ),
               )
-            );*/
-            //通过命名路由跳转页面llllllllllll
-            Navigator.pushNamed(context, '/form');
-          },
+            ],
+          ),
         ),
-        RaisedButton(
-          child: Text('点击注册'),
-          onPressed: (){
-            Navigator.pushNamed(context, '/register');
-          },
-        )
-      ],
+        body: TabBarView(
+          children: [
+            Column(
+              children: [
+                RaisedButton(
+                  child: Text('进入表单界面'),
+                  onPressed: (){
+                    /*Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => FormPage(
+                            title: '我是传值数据',
+                          )
+                        )
+                      );*/
+                    //通过命名路由跳转页面llllllllllll
+                    Navigator.pushNamed(context, '/form');
+                  },
+                ),
+                RaisedButton(
+                  child: Text('点击注册'),
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/register');
+                  },
+                )
+              ],
+            ),
+            ListView(
+              children: [
+                ListTile(
+                  title: Text('secondTab'),
+                )
+              ],
+            ),
+            ListView(
+              children: [
+                ListTile(
+                  title: Text('直播'),
+                )
+              ],
+            ),
+            ListView(
+              children: [
+                ListTile(
+                  title: Text('社区'),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
