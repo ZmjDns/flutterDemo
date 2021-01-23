325834a8446fb5986a7dd645eff33accdf6e3db6
ZmjDns <292570927@qq.com>
2021-01-23 22:49:55 +0800

    单选框CheckBox

--- lib/pages/CheckBoxRadioBtnPage.dart
+++ lib/pages/CheckBoxRadioBtnPage.dart
@@ -7,6 +7,8 @@ class CheckBoxRadioBtnPage extends StatefulWidget {
 }
 
 class _CheckBoxRadioBtnPageState extends State<CheckBoxRadioBtnPage> {
+
+  var isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +18,46 @@ class _CheckBoxRadioBtnPageState extends State<CheckBoxRadioBtnPage> {
       body: Padding(
         padding: EdgeInsets.all(20),
         child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('单选多选框')
+            Text('单选多选框'),
+            Row(
+              children: [
+                Checkbox(
+                  value: this.isChecked,
+                  onChanged: (v){
+                    setState(() {
+                      this.isChecked = v;
+                    });
+                  },
+                  activeColor: Colors.red,
+                ),
+                Text(this.isChecked ? '选中' : '未选中')
+              ],
+            ),
+            Divider(),
+            CheckboxListTile(
+              value: this.isChecked,
+              onChanged: (v){
+                setState(() {
+                  this.isChecked = v;
+                });
+              },
+              title: Text('标题'),
+              subtitle: Text('子标题'),
+            ),
+            Divider(),
+            CheckboxListTile(
+              value: this.isChecked,
+              onChanged: (v){
+                setState(() {
+                  this.isChecked = v;
+                });
+              },
+              title: Text('标题'),
+              subtitle: Text('二级标题'),
+              secondary: Icon(Icons.help),
+            )
           ],
         ),
       )
