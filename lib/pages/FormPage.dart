da737c1deeb80f9cb196e8c940e9bb2478f40191
ZmjDns <292570927@qq.com>
2021-01-23 21:34:30 +0800

    表单输入的简单使用，及封装

--- lib/pages/FormPage.dart
+++ lib/pages/FormPage.dart
@@ -15,11 +15,71 @@ class FormPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(this.title),
       ),
-      body: Column(
+      body: TextFields(),
+    );
+  }
+}
+
+
+class TextFields extends StatefulWidget {
+  @override
+  _TextFieldsState createState() => _TextFieldsState();
+}
+
+class _TextFieldsState extends State<TextFields> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
         children: [
-          Text('This is from page')
+          Text('This is from page'),
+          TextField(),
+          SizedBox(height: 10),
+          TextField(
+            decoration: InputDecoration(
+                hintText: '请输入.....',
+                border: OutlineInputBorder()
+            ),
+          ),
+          SizedBox(height: 10,),
+          TextField(
+            maxLines: 4,
+            decoration: InputDecoration(
+                hintText: '多行文本框',
+                border: OutlineInputBorder()
+            ),
+          ),
+          SizedBox(height: 20,),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                hintText: '密码框',
+                border: OutlineInputBorder()
+            ),
+          ),
+          SizedBox(height: 10,),
+          TextField(
+            decoration: InputDecoration(
+                hintText: '用户',
+                labelText: '用户名',
+                border: OutlineInputBorder()
+            ),
+          ),
+          SizedBox(height: 10,),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                icon: Icon(Icons.people),
+                hintText: '密码',
+                labelText: '密码',
+                border: OutlineInputBorder()
+            ),
+          )
         ],
       ),
     );
   }
 }
+
