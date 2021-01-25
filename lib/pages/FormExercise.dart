0dfe35770906eb375b829c673a1e745c404488b1
ZmjDns <292570927@qq.com>
2021-01-25 20:40:17 +0800

    radioBtn练习

--- lib/pages/FormExercise.dart
+++ lib/pages/FormExercise.dart
@@ -17,6 +17,11 @@ class _FormExerciseState extends State<FormExercise> {
   //   this.name.text = '';
   // }
 
+  void _sexChanged(value) {
+    setState(() {
+      this.sex = value;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,15 +42,22 @@ class _FormExerciseState extends State<FormExercise> {
             Row(
               children: [
                 Text('男'),
-                Radio(value: 1, groupValue: this.sex, onChanged: (v){setState(() {
-                  this.sex = v;
-                });}),
+                Radio(value: 1, groupValue: this.sex, onChanged: this._sexChanged), //  注意这里的this._sexChanged是将此方法赋给onChanged属性而不是调用
                 SizedBox(width: 20,),
                 Text('女'),
-                Radio(value: 2, groupValue: this.sex, onChanged: (v){setState(() {
-                  this.sex = v;
-                });})
+                Radio(value: 2, groupValue: this.sex, onChanged: this._sexChanged)
               ],
+            ),
+            Container(
+              width: double.infinity,
+              height: 40,
+              child: RaisedButton(
+                child: Text('提交'),
+                onPressed: (){
+                  print(this.name.text);
+                  print(this.sex);
+                },
+              ),
             )
           ],
         ),
