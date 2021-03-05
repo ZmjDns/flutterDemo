cbd6de6f12dab4d5319e2e55acc7e7cbf86c3fe1
ZmjDns <292570927@qq.com>
2021-03-05 16:54:40 +0800

    三方组件实现datePicker存在问题

--- lib/pages/DatePage.dart
+++ lib/pages/DatePage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:date_format/date_format.dart';
+import 'package:flutter_holo_date_picker/date_picker.dart';
+import 'package:flutter_holo_date_picker/flutter_holo_date_picker.dart';
+import 'package:flutter_holo_date_picker/i18n/date_picker_i18n.dart';
 
 class DatePage extends StatefulWidget {
   @override
@@ -68,6 +71,40 @@ class _DatePageState extends State<DatePage> {
     });
   }
 
+
+  DateTime _dateTimeThirdParty = DateTime.now();
+  //三方组件
+  _showThirdPartyDatePicker () {
+    print('............');
+    // DatePicker.showDatePicker(
+    //   context,
+    //   onMonthChangeStartWithFirstDate: true,
+    //   pickerTheme: DateTimePickerTheme(
+    //     showTitle: true,
+    //     confirm: Text('确定', style: TextStyle(color: Colors.red)),
+    //   ),
+    //   minDateTime: DateTime.parse('1980-05-12'),
+    //   maxDateTime: DateTime.parse('2100-05-12'),
+    //   initialDateTime: _dateTimeThirdParty,
+    //   dateFormat: 'yyyy-mm-dd',
+    //   locale: DateTimePickerLocale.zh_cn,
+    //   onCancel: () {
+    //     debugPrint('cancel......');
+    //   },
+    //   // onChange: (dateTime, List<int> index) {
+    //   //
+    //   // },
+    //   onConfirm: (dateTime, List<int> index) {
+    //     setState(() {
+    //       this._dateTimeThirdParty = dateTime;
+    //     });
+    //   }
+    // );
+
+
+  }
+
+  DateTime _selectedDate;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,8 +146,44 @@ class _DatePageState extends State<DatePage> {
                 },
               )
             ],
-          )
+          ),
 
+
+          //三方组件
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('第三方时间组件')
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('2021-01-26'),
+                    Icon(Icons.arrow_drop_down)
+                  ],
+                ),
+                onTap: () async {
+                  var datePicked = await DatePicker.showSimpleDatePicker(
+                    context,
+                    initialDate: DateTime(1994),
+                    firstDate: DateTime(1960),
+                    lastDate: DateTime(2050),
+                    dateFormat: 'dd-MMMM-yyyy',
+                    locale: DateTimePickerLocale.en_us,
+                    looping: true
+                  );
+                  final snackBar = SnackBar(content: Text('Date Picked $datePicked'));
+                  Scaffold.of(context).showSnackBar(snackBar);
+                },
+              ),
+              Text('几点了')
+            ],
+          ),
         ],
       ),
     );
