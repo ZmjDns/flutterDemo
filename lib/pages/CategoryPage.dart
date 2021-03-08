04d46fecd4269bd9b6ed37ae7f6547edcfb6f6bf
ZmjDns <292570927@qq.com>
2021-03-08 22:38:19 +0800

    l

--- lib/pages/CategoryPage.dart
+++ lib/pages/CategoryPage.dart
@@ -49,7 +49,7 @@ class _CategoryPageState extends State<CategoryPage> {
                           )
                         )
                       );*/
-                    //通过命名路由跳转页面lllllllllllllllllllllllllllllllllllllllllllllllll
+                    //通过命名路由跳转页面llllllllllllllllllllllllllllllllllllllllllllllllll
                     Navigator.pushNamed(context, '/form');
                   },
                 ),
