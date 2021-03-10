fc42257222e2298f4d40db0c34a0295876b1f90e
ZmjDns <292570927@qq.com>
2021-03-10 10:17:23 +0800

    new style轮播

--- lib/pages/SwiperPage.dart
+++ lib/pages/SwiperPage.dart
@@ -45,7 +45,26 @@ class _SwiperPageState extends State<SwiperPage> {
               children: [
                 Text('我是文字'),
               ],
-            )
+            ),
+            Container(
+                child: Swiper(
+                    layout: SwiperLayout.CUSTOM,
+                    customLayoutOption:
+                    new CustomLayoutOption(startIndex: -1, stateCount: 3)
+                        .addRotate([-45.0 / 180, 0.0, 45.0 / 180])
+                        .addTranslate([new Offset(-370.0, -40.0),new Offset(0.0, 0.0),new Offset(370.0, -40.0)]),
+                    itemWidth: 300.0,
+                    itemHeight: 200.0,
+                    itemBuilder: (context, index) {
+                      return new Container(
+                        // color: Colors.grey,
+                        child: new Center(
+                          child: Image.network(imgList[index]['url'], fit: BoxFit.cover,),
+                        ),
+                      );
+                    },
+                    itemCount: imgList.length)
+            ),
           ],
         ));
   }
