a3ff5055c06dfc41f0cb103c161d810597379b39
ZmjDns <292570927@qq.com>
2021-03-10 09:52:59 +0800

    格式化code，实现自动轮播

--- lib/pages/SwiperPage.dart
+++ lib/pages/SwiperPage.dart
@@ -16,31 +16,37 @@ class _SwiperPageState extends State<SwiperPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('轮播图'),),
-      body: Column(
-        children: [
-          Container(
-            // height: 200, //(去掉高度)
-            width: double.infinity,//(实现宽度100%平铺)
-            child: AspectRatio( //  限定比例
-              aspectRatio: 16/9,
-              child: new Swiper(
-                itemBuilder: (BuildContext context,int index){
-                return new Image.network(imgList[index]['url'],fit: BoxFit.fill,);
-                },
+        appBar: AppBar(
+          title: Text('轮播图'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              // height: 200, //(去掉高度)
+              width: double.infinity, //(实现宽度100%平铺)
+              child: AspectRatio(
+                //  限定比例
+                aspectRatio: 16 / 9,
+                child: new Swiper(
+                  itemBuilder: (BuildContext context, int index) {
+                    return new Image.network(
+                      imgList[index]['url'],
+                      fit: BoxFit.fill,
+                    );
+                  },
                   itemCount: imgList.length,
                   pagination: new SwiperPagination(), //分页
-                  control: new SwiperControl(), //  滑动箭头
+                  // control: new SwiperControl(), //  滑动箭头
+                  autoplay: true,
                 ),
+              ),
             ),
-          ),
-          Row(
-            children: [
-              Text('我是文字'),
-            ],
-          )
-        ],
-      )
-    );
+            Row(
+              children: [
+                Text('我是文字'),
+              ],
+            )
+          ],
+        ));
   }
 }
