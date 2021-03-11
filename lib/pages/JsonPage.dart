a16c454313baf7b00146a4fec9140e49525f6a51
ZmjDns <292570927@qq.com>
2021-03-11 16:05:02 +0800

    dart内置json将JSONstring与Map相互转换

--- lib/pages/JsonPage.dart
+++ lib/pages/JsonPage.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class JsonPage extends StatefulWidget {
@@ -6,6 +8,27 @@ class JsonPage extends StatefulWidget {
 }
 
 class _JsonPageState extends State<JsonPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    _encodeMap2Json();
+    _decodeJson2Map();
+  }
+
+  _encodeMap2Json() {
+    Map user = {
+      'userName': '张三',
+      'age': 20
+    };
+    print(json.encode(user));
+  }
+  _decodeJson2Map() {
+    String jsonStr = '{"userName": "ZhangSan","age":20}';
+    Map user = json.decode(jsonStr);
+    print(user['userName'] + user['age'].toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
