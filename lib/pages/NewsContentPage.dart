f2866faad0dfae35c69c8c8d3941e84bad0b0720
ZmjDns <292570927@qq.com>
2021-03-23 16:20:22 +0800

    引入flutter_html，实现Html对html内容的解析显示

--- lib/pages/NewsContentPage.dart
+++ lib/pages/NewsContentPage.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 class NewsContentPage extends StatefulWidget {
   Map arguments;
@@ -8,13 +13,52 @@ class NewsContentPage extends StatefulWidget {
 }
 
 class _NewsContentPageState extends State<NewsContentPage> {
+
+  List _dataList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    print('传来数据:${widget.arguments}');
+    _getHtmlData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('商品详情'),
       ),
-      body: Text('传来数据${widget.arguments}'),
+      body: Container(
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: _dataList.length,
+          itemBuilder: (context,index) {
+            return Column(
+              children: [
+                ListTile(
+                  title: Text(_dataList[index]['summary'],maxLines: 2,),
+                ),
+                Html(
+                  data: _dataList[index]['content'],
+                )
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
+
+  void _getHtmlData () async {
+    var api = 'http://www.phonegap100.com/appapi.php?a=getPortalArticle&aid=20';
+    var response = await Dio().get(api);
+    setState(() {
+      _dataList = json.decode(response.data)['result'];
+      print('返回数据：${_dataList[0]}');
+    });
+
+  }
+
+
 }
